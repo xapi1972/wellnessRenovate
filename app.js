89720924c3685d490c7c3ba43c6ba6f38203b96f
Albert Farre <afarre@sicteltelecom.com>
2018-10-16 21:10:04 +0200

    packet express-session and bcrypt installed, definition of routes for auth

--- app.js
+++ app.js
@@ -3,16 +3,34 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const session = require('express-session');
+
+const auth = require('./routes/auth')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(session({
+  secret: 'react auth secret shh',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { httpOnly: true, maxAge: 60000 },
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
